f9cdbbad3acce3c861fb3fbaade1a1a4ce16a1f1
hexhooves <admin@thetrotspot.net>
2014-07-22 05:52:32 -0400

    Added Latest Compiled DLLs

--- THEMEFIX/Form1.vb
+++ THEMEFIX/Form1.vb
@@ -1,6 +1,11 @@
 ﻿Public Class Form1
 
     Private Sub NsButton1_Click(sender As Object, e As EventArgs) Handles NsButton1.Click
-        NsListView1.Items.Add("TEST")
+        Dim Temp As New NetSeal.NSListView.NSListViewItem
+        Dim Temp2 As New NetSeal.NSListView.NSListViewSubItem
+        Temp2.Text = "W00T"
+        Temp.SubItems.Add(Temp2)
+        Temp.Text = "Top KeK"
+        NsListView1.Items.Add(Temp)
     End Sub
 End Class
